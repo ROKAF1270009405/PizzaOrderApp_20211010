9d528bbd6e61cbaee2193a0cc36493e233127f3a
전상효 <junsanghyo0820@gmail.com>
2021-10-10 11:05:02 +0900

    뷰페이저 / 탭레이아웃 화면 출현

--- app/src/main/java/com/jayshe/pizzaorderapp_20211010/MainActivity.kt
+++ app/src/main/java/com/jayshe/pizzaorderapp_20211010/MainActivity.kt
@@ -2,10 +2,23 @@ package com.jayshe.pizzaorderapp_20211010
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jayshe.pizzaorderapp_20211010.adapters.MainViewPageAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainViewPageAdapter: MainViewPageAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainViewPageAdapter = MainViewPageAdapter(supportFragmentManager)
+        mainViewPager.adapter = mainViewPageAdapter
+
+        mainTabLayout.setupWithViewPager(mainViewPager)
+
+
+
     }
 }
