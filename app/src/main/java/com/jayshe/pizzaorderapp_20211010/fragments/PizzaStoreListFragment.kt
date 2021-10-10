97fdd3d6fef997062bef433d28971c59aeb9a2a9
전상효 <junsanghyo0820@gmail.com>
2021-10-10 13:46:36 +0900

    가게 누르면 상세화면 이동   - Serializable 활용 데이터 첨부

--- app/src/main/java/com/jayshe/pizzaorderapp_20211010/fragments/PizzaStoreListFragment.kt
+++ app/src/main/java/com/jayshe/pizzaorderapp_20211010/fragments/PizzaStoreListFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.jayshe.pizzaorderapp_20211010.R
+import com.jayshe.pizzaorderapp_20211010.ViewStoreDetailActivity
 import com.jayshe.pizzaorderapp_20211010.adapters.PizzaStoreAdapter
 import com.jayshe.pizzaorderapp_20211010.datas.StoreData
 import kotlinx.android.synthetic.main.fragment_pizza_store_list.*
@@ -39,7 +40,8 @@ class PizzaStoreListFragment : Fragment() {
 
             val clickedStore = mPizzaStoreList[position]
 
-//            val myIntent = Intent(requireContext(), )
+            val myIntent = Intent(requireContext(), ViewStoreDetailActivity::class.java)
+            myIntent.putExtra("store",clickedStore)
 
         }
 
